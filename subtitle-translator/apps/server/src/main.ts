4840287fb27277ff8974629e76e8de0ea87dc498
Vincent <vcapic@gmail.com>
2023-12-10 07:58:24 +0100

    init

--- subtitle-translator/apps/server/src/main.ts
+++ subtitle-translator/apps/server/src/main.ts
@@ -30,7 +30,7 @@ app.post('/api', (req, res) => {
     //Use the mv() method to place the file in the upload directory (i.e. "uploads")
     file.mv('/data/temp/' + file.name);
     console.error('File uploaded')
-    exec(`cd /home/capic/docker/translator_watcher/data/temp/ && mkvextract tracks ${file.name} 2:${file.name}.srt`, (err, stdout, stderr) => {
+    exec(`cd /data/temp/ && mkvextract tracks ${file.name} 2:${file.name}.srt`, (err, stdout, stderr) => {
       if (err) {
         //some err occurred
         console.error(err)
