a4556496cf4f6c803764f4da10d22d2c2a833498
Vincent <vcapic@gmail.com>
2023-12-18 08:06:33 +0100

    init

--- subtitle-translator/apps/client/src/app/App2.tsx
+++ subtitle-translator/apps/client/src/app/App2.tsx
@@ -1,26 +1,32 @@
-import axios from "axios"
-import { useQuery } from "react-query"
-import React from "react"
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import React from 'react';
 
 const fetchFiles = async () => {
-    const resp = await axios.get<{name: string, path: string}[]>('http://192.168.1.106:3333/api/files')
-    return resp
-}
+  const resp = await axios.get<{ name: string; path: string }[]>(
+    'http://192.168.1.106:3333/api/files'
+  );
+  return resp;
+};
 
 const App2 = () => {
-    const {data, error, isFetching} = useQuery('files', fetchFiles)
+  const { data, error, isFetching } = useQuery('files', fetchFiles);
 
-    if (isFetching) {
-        return <>Loading....</>
-    }
+  if (isFetching) {
+    return <>Loading....</>;
+  }
 
-    if(error) {
-        return <>{error}</>
-    }
+  if (error) {
+    return <>{error}</>;
+  }
 
-    return (<ul>
-        <li><button onClick={}>{data?.data.map(directory => directory.name)}</button></li>
-    </ul>)
-}
+  return (
+    <ul>
+      <li>
+        <button>{data?.data.map((directory) => directory.name)}</button>
+      </li>
+    </ul>
+  );
+};
 
-export default App2
+export default App2;
