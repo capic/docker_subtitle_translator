699ed80c41c8fcd20c60c2953250da329b1d225b
Vincent <vcapic@gmail.com>
2023-12-22 21:44:30 +0100

    init

--- subtitle-translator/apps/server/src/main.ts
+++ subtitle-translator/apps/server/src/main.ts
@@ -100,8 +100,12 @@ app.post('/api/translate', (req, res) => {
         // the *entire* stdout and stderr (buffered)
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
-        fs.copyFileSync(`/data/temp/${path.basename(filePath)}.srt`,
-        `/data/input/${path.basename(filePath)}.srt`)
+        /* fs.copyFileSync(`/data/temp/${path.basename(filePath)}.srt`,
+        `/data/input/${path.basename(filePath)}.srt`) */
+        exec(`subtrans translate "/data/temp/${path.basename(
+          filePath
+        )}.srt" --src en --dest fr`)
+
         fs.rmSync(`/data/temp/${path.basename(filePath)}.srt`)
       }
     }
