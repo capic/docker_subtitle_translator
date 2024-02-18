e1204bae46b5c7b912a8cb8dbcc9fe98ee7f139e
Vincent <vcapic@gmail.com>
2024-02-18 15:45:37 +0100

    use fastify instead of express

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ python src/main.py /data/input/ /data/output/ &
 
 # Start the helper process
 cd subtitle-translator
-npx nx serve server --configuration production &
+npx nx serve api --configuration production &
 NODE_ENV=production npx nx run client:preview --host 0.0.0.0 --disableHostCheck &
 
 # the my_helper_process might need to know how to wait on the
